8950c0742cc59d12b102f71e976ca6d3a6e05fb6
IntelLegend <odudujohn@yahoo.com>
2022-09-08 14:51:12 -0700

    The C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	puts("Programming is like building a miltilingial puzzle");
-	return(0);
+
+	puts("\"Programming is like building a miltilingial puzzle");
+	return (0);
+
 }
+
