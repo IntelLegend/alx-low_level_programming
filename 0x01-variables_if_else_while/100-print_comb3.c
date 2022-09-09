4afc8f7e402788c85228598fb3cdcc51059ff10d
IntelLegend <odudujohn@yahoo.com>
2022-09-09 13:31:16 -0700

    C programming continues

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,46 +1,40 @@
-include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
 /**
- * main - Entry point
+ * main - print single digit numbers
+ *
+ * Description: print single digit numbers with commas
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int i;
-	int j;
+	int i, j;
 
 	i = 48;
 	j = 48;
-	while  ((i < 58) && (j < 58))
+
+	while (i < 58)
 	{
-		putchar(i);
-		putchar(j);
-		if ((i == 57) && (j == 57))
+		j = 48;
+		while (j < 58)
 		{
-			putchar('\n');
-			i++;
+			putchar(i);
+			putchar(j);
+
+			if (i < 57 || j < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
 			j++;
 		}
-		else
-		{
-			putchar(44);
-			putchar(32);
-			if (j < 57)
-			{
-				j++;
-			}
-			else
-			{
-				j = 48;
-				i++;
-			}
-		}
 
+		i++;
 	}
+
+	putchar(10);
 
 	return (0);
 }
