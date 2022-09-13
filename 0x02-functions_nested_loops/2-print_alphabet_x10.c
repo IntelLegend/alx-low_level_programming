5e46d13061c601237f63845b902036922ea17e41
IntelLegend <odudujohn@yahoo.com>
2022-09-14 00:25:25 +0100

    monday project on alx

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ *print_alphabet - substitute for main function
+ *Return: what my code is expected to return
+ */
+
+void print_alphabet(void)
+{
+	int i;
+	char a = 'a';
+	for (i = 0; i < 10; 1++)
+	{
+		while (a = 'a')
+		{
+			_putchar(a);
+			a++
+		}
+	}
+	_putchar(10);
+}
