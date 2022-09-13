b6e27726f753131ea28ba902648bbeff79cba572
IntelLegend <odudujohn@yahoo.com>
2022-09-13 23:14:59 +0100

    monday project on alx

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program execution begins here
+ * Return:  the return value
+ * Description: Program that displays a to z
+ */
+
+int main(void)
+{
+	char a=97;
+
+	while (a<=122)
+	{
+		printf("%c\n", a);
+		a++;
+	}
+}
