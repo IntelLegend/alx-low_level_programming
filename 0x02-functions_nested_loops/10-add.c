b1a693a2baf40b43300f1a4e66f78b9ea84c35f4
IntelLegend <odudujohn@yahoo.com>
2022-09-14 21:13:58 +0100

    monday project on alx

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - do addition
+ * @a: added operand
+ * @b: added operand
+ * Return: returns 0 for a successful code
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
