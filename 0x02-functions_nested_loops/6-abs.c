56b6022aa3d54c2dfe391edbd4c2eef59fc75f78
IntelLegend <odudujohn@yahoo.com>
2022-09-14 20:59:36 +0100

    monday project on alx

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+
+#include "main.h"
+/**
+ * _abs - displays the absolute value of given numbers
+ * @value: a supposed vslue
+ * Return: to return 0 fdor a successful code
+ */
+
+int _abs(int value)
+{
+	if (value < 0)
+	{
+		return (-(value));
+	}
+	else
+	{
+		return  (value);
+	}
+}
