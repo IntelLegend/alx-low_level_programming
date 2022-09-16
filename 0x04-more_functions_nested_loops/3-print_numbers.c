17fb128e7786b2ac667499a261a34bb464d5f22c
IntelLegend <odudujohn@yahoo.com>
2022-09-16 03:28:52 +0100

    More on functions and nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints digit
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar('0' + i);
+		i++;
+	}
+	_putchar('\n');
+}
