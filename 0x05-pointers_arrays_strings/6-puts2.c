94e38e1159006f0fb07726382ffda784e61409de
IntelLegend <odudujohn@yahoo.com>
2022-09-20 03:15:19 +0100

    Doing pointers in C with ALX

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - display string
+ * @str: string variable
+ */
+
+void puts2(char *str)
+{
+	int h = 0;
+	int i;
+
+	while (str[c] != '\0')
+	{
+		h++;
+	}
+	for (i = 0; i < h; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
