64fb4a31af8eee761614f6dc7e9cf9b8391d0492
IntelLegend <odudujohn@yahoo.com>
2022-09-20 02:02:17 +0100

    Doing pointers in C with ALX

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap-int - where my code begins
+ *@a: value given
+ *@b: Swapped value
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	d = *a;
+	*a = *b;
+	*b = c;
+}
+
