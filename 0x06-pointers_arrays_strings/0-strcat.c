65ad8994e4d3891820e25981f76b1ebfd9800150
IntelLegend <odudujohn@yahoo.com>
2022-09-23 02:28:05 +0100

    more on strings, pointers and arrays

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -17,7 +17,7 @@ char *_strcat(char *dest, char *src)
 		k++;
 	}
 	for (j = 0 ; j < n && src[j] != '\0' ; j++)
-		dest[n + j] = src[j];
+		dest[k + j] = src[j];
 	dest[k + j] = '\0';
 
 	return (dest);
