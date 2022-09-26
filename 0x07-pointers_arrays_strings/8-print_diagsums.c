38914456555577a87c6b6e67e4940b1c01b6ee9c
IntelLegend <odudujohn@yahoo.com>
2022-09-26 20:38:45 +0100

    Pointing to a pointer

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sums of the two diagonals
+ * @a: array
+ * @size: size of array
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum1 =  0;
+	int sum2 = 0;
+	int fmr = 0;
+
+	for (i = 0; i < size * size; i++)
+	{
+		if (i == 0)
+		{
+			sum1 += *(a + i);
+			fmr = i;
+		}
+		else if (i == (prev + size + 1))
+		{
+			sum1 += *(a + i);
+			fmr = i;
+		}
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		sum2 += *(a + (size * (i + 1) - (i + 1)));
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
