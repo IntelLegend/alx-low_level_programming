f36429e28e9afe7c7883f7a432dc379ad33e27bf
IntelLegend <odudujohn@yahoo.com>
2022-09-29 04:02:06 +0100

    Recursion with ALX

---
+++ 0x08-recursion/6-is-prime_number.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * prime - prime number
+ * @n: int
+ * @y: int
+ * Return: 1 for prime number
+ */
+int prime(int n, int y)
+{
+	if (n % y == 0)
+	{
+		return (0);
+	}
+	if (y >= n / 2)
+	{
+		return (1);
+	}
+	return (prime(n, y + 1));
+}
+
+/**
+ * is_prime_number - displays prime
+ * @n: int
+ * Return: 1 on success
+ */
+int is_prime_number(int n)
+{
+	if (n < 0)
+	{
+		return (0);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	return (prime(n, 2));
+}
