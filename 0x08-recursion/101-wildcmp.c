fa511b1133f984ab0925fc8eef5e2d798efecee6
IntelLegend <odudujohn@yahoo.com>
2022-09-29 04:49:12 +0100

    Recursion with ALX

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * wildcmp- compare string and return if strings are identical
+ * @s1: pointer
+ * @s2: pointer
+ * Return: 1 if identical
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+			return (wildcmp(s1, s2 + 1));
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
+}
